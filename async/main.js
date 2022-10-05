4ad1b6a54094ad7a12504d8b2155092fe9c4bd17
junb119 <emfowkd@gmail.com>
2022-10-05 16:56:57 +0900

    비동기 - 예외처리(then,catch,finally) 2

--- async/main.js
+++ async/main.js
@@ -1,45 +1,41 @@
-function a() {
-  return new Promise((resolve) => {
+function a(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 4){
+      reject()
+      return
+    }
     setTimeout(()=>{
       console.log('A')
       resolve()
     },1000)
   })
 }
-function b() {
-  return new Promise((resolve) => {
-    setTimeout(()=>{
-      console.log('B')
-      resolve()
-    },1000)
-  })
-}
-function c() {
-  return new Promise((resolve) => {
-    setTimeout(()=>{
-      console.log('C')
-      resolve()
-    },1000)
-  })
-}
-function d() {
-  return new Promise((resolve) => {
-    setTimeout(()=>{
-      console.log('D')
-      resolve()
-    },1000)
-  })
-}
 // async, await 대체
 // then,catch,finally : primise 객체에서 사용
-function test() {
-  a()
-  .then(() => b())
-  .then(() => c())
-  .then(() => d())
-  .then(() => {
-    console.log('Done')
-  })
+async function test() {
+  
+  // a(7)
+  //   .then(() => {
+  //     console.log('Resolve!')
+  //   })
+  //   .catch(() => {
+  //     console.log('Reject!')
+  //   })
+  //   .finally(() =>{
+  //     console.log('Done!')
+  //   })
+
+
+  try {
+    await a(7)
+    console.log('Resolve!')
+  
+  } catch (error) {
+    console.log('Reject!')
+  } finally {
+    console.log('Done!')
+  }
+
 }
 test()
 
