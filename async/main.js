73c1a47b281eb855ba6bb2a79b496a4e11346588
junb119 <emfowkd@gmail.com>
2022-10-05 17:57:12 +0900

    비동기 - API 비동기 처리 연습

--- async/main.js
+++ async/main.js
@@ -1,43 +1,36 @@
-function a(number) {
-  return new Promise((resolve, reject) => {
-    if (number > 4){
-      reject()
-      return
+// 비동기 패턴
+
+function fetchMovies(title) {
+  // 대기(pending) : 이행하거나 거부되지 않은 초기 상태
+  const OMDB_API_KEY = '7035c60c123'
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await axios.get(`https://omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}`)
+  // 이행(fulfilled) : 연산이 성공적으로 완료됨.
+      resolve(res)
+    } catch(hello) {
+      console.log(hello.message)
+      // 거부(rejected) : 연산이 실패함
+      reject('HEROPY?!')
     }
-    setTimeout(()=>{
-      console.log('A')
-      resolve()
-    },1000)
   })
 }
-// async, await 대체
-// then,catch,finally : primise 객체에서 사용
+
 async function test() {
-  
-  // a(7)
-  //   .then(() => {
-  //     console.log('Resolve!')
-  //   })
-  //   .catch(() => {
-  //     console.log('Reject!')
-  //   })
-  //   .finally(() =>{
-  //     console.log('Done!')
-  //   })
-
-
   try {
-    await a(7)
-    console.log('Resolve!')
+    const res =  await fetchMovies('frozen')
+    console.log(res)
   
-  } catch (error) {
-    console.log('Reject!')
-  } finally {
-    console.log('Done!')
+  } catch (heropy) {
+    console.log(heropy)
   }
-
-}
+  }
 test()
 
+function hello(){
+  fetchMovies('jobs')
+  .then(res => console.log(res))
+  .catch(heropy => {console.log(heropy)})
+}
 
-
+hello()
