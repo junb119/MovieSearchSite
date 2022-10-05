d100c161b760027fd6e55556e0f79d509f32ed64
junb119 <emfowkd@gmail.com>
2022-10-05 16:49:44 +0900

    비동기 - 예외처리(then,catch,finally) 1

--- async/main.js
+++ async/main.js
@@ -1,55 +1,47 @@
 function a() {
-  // promise : 약속의 객체
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
       console.log('A')
-      resolve('Hello A') // resovle : 매개변수를 실행할 위치
-    }, 1000)
-  })
-}
-function a() {
-  // promise : 약속의 객체
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      console.log('A')
-      resolve('Hello A') // resovle : 매개변수를 실행할 위치
-    }, 1000)
+      resolve()
+    },1000)
   })
 }
 function b() {
-  // promise : 약속의 객체
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
       console.log('B')
-      resolve('Hello B') // resovle : 매개변수를 실행할 위치
-    }, 1000)
+      resolve()
+    },1000)
   })
 }
 function c() {
-  // promise : 약속의 객체
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
       console.log('C')
-      resolve('Hello C') // resovle : 매개변수를 실행할 위치
-    }, 1000)
+      resolve()
+    },1000)
   })
 }
 function d() {
-  // promise : 약속의 객체
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise((resolve) => {
+    setTimeout(()=>{
       console.log('D')
-      resolve('Hello D') // resovle : 매개변수를 실행할 위치
-    }, 1000)
+      resolve()
+    },1000)
   })
 }
-
-async function test(){
-  const h1 = await a() // A
-  const h2 = await b() // B
-  const h3 = await c() // C
-  const h4 = await d() // D
-  console.log('Done!') // Done!
-  console.log(h1,h2,h3,h4) // 'Hello A','Hello B','Hello C','Hello D',
+// async, await 대체
+// then,catch,finally : primise 객체에서 사용
+function test() {
+  a()
+  .then(() => b())
+  .then(() => c())
+  .then(() => d())
+  .then(() => {
+    console.log('Done')
+  })
 }
 test()
+
+
+
