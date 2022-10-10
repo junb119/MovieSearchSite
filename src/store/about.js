e07624247f996c95ca0e6d112af2b7d4155315c2
junb119 <emfowkd@gmail.com>
2022-10-10 17:31:26 +0900

    About

--- src/store/about.js
+++ src/store/about.js
@@ -1,1 +1,12 @@
 // 개발자, 사용자 정보 취급
+
+export default {
+  namespaced: true,
+  state : () => ({
+    name: 'HEROPY',
+    email: 'emfowkd@gmail.com',
+    blog: 'https://heropy.blog',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })  
+}
