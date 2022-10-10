cd8489acb29d02f8bb8b4160cedb1dae274746c9
junb119 <emfowkd@gmail.com>
2022-10-10 16:39:58 +0900

    vue플러그인(이미지 로드 이벤트)

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index.js'
 import store from './store/index.js' // index.js는 생략가능 (=>./store)
+import loadImage from './plugins/loadImage'
 
 createApp(App)
-.use(router)
-.use(store)
+.use(router)  // $route, $router
+.use(store) // $store
+.use(loadImage) // $loadImage
 .mount('#app')
 
